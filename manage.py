c16cd99b31a2d122115f98b05d2daea832c3fc89
vince lynch <root@vps307308.ovh.net>
2017-06-06 19:09:27 +0100

    ...

---
+++ manage.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+import json
+
+data = []
+with open('conversionList.json', 'r') as f:
+     data = json.load(f)
+
+
+for convert_this in data['toConvert']:
+	 print ("converting -- " + convert_this['filename'])
+	 if hasattr(convert_this, "outFormat"):
+	 	os.system("convert "+convert_this['filename']+" -resize "+convert_this['resize']+" " + convert_this['outFormat'] + ":" + convert_this['saveAs']) 
+	 else:
+		 os.system("convert "+convert_this['filename']+" -resize "+convert_this['resize']+" " + convert_this['saveAs']) 
+
